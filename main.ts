068a0cb3dd12f3ab8e5685e0bef2560088fb092e
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-05-12 09:45:47 +0200

    first commit but not

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
+radio.setGroup(12)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(50)
 
+type drivingSignal = {
+    x: number;
+    y: number;
+    z: number
+}
+
+let safeKey = "123ABC"
+let drivingPackage: drivingSignal = { x: 0, y: 0, z: 0 }
+
+//control.deviceSerialNumber()
+//radio.setTransmitSerialNumber(true)
+
+basic.forever(function () {
+    drivingPackage.x += 1
+    basic.showNumber(drivingPackage.x)
+    basic.pause(20)
+})
