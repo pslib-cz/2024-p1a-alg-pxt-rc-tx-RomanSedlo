e2d64af855484ff7c0c2a9d51a977a3863b4f4f0
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-05-16 13:32:46 +0200

    tada

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 radio.setGroup(12)
 radio.setTransmitPower(7)
-radio.setFrequencyBand(50)
+radio.setFrequencyBand(39)
 radio.setTransmitSerialNumber(true)
-
+control.deviceSerialNumber()
 
 type drivingSignal = {
     x: number;
@@ -13,19 +13,18 @@ type drivingSignal = {
 let drivingPackage: drivingSignal = {x: 0, y: 0, z: 0 }
 let ready = false
 let serialNumber = control.deviceSerialNumber()
-let key = 1234
-let encodedSerialNumber = serialNumber ^ key
 
-radio.sendValue("data", encodedSerialNumber)
 
 function calibrate() {
     drivingPackage = { x: 0, y: 0, z: 0 }
+    music.play(music.stringPlayable("A3 C4 E4 A4", 200), music.PlaybackMode.UntilDone)
     basic.showString("X")
 }
 
 
 input.onButtonPressed(Button.A, function () {
     ready = true
+    music.play(music.stringPlayable("C4 E4 G4 C5", 200), music.PlaybackMode.UntilDone)
 })
 
 input.onButtonPressed(Button.B, function () {
@@ -37,6 +36,9 @@ input.onButtonPressed(Button.B, function () {
 basic.forever(function() {
     if(ready) {
         basic.showString("R")
+        drivingPackage.x = input.acceleration(Dimension.X)
+        drivingPackage.y = input.acceleration(Dimension.Y)
+        drivingPackage.z = input.acceleration(Dimension.Z)
         drivingPackage = { x: 1, y: 1, z: 1 }
         radio.sendString(JSON.stringify(drivingPackage))
     }
