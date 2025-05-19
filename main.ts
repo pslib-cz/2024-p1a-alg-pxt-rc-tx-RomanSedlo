c854921f9b36fc8daa9761166d895680cd2e880c
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-05-19 21:22:14 +0200

    WIGELY EYES

--- main.ts
+++ main.ts
@@ -4,19 +4,17 @@ radio.setFrequencyBand(39)
 radio.setTransmitSerialNumber(true)
 
 type drivingSignal = {
-    k: number;
     x: number;
     y: number;
     z: number
 }
 
-let keySqequence: Array<number> = [4, 8, 6, 1, 5, 3, 4, 7, 2, 6, 0, 3, 9, 4, 8, 2, 6, 3, 7, 5, 2, 1, 0, 6, 8, 3, 5, 9];
-
-let drivingPackage: drivingSignal = { k: keySqequence[0], x: 0, y: 0, z: 0 }
+let drivingPackage: drivingSignal = { x: 0, y: 0, z: 0 }
+let stringPackage: string
 let ready = false
 
 function calibrate() {
-    drivingPackage = { k: keySqequence[0], x: 0, y: 0, z: 0 }
+    drivingPackage = { x: 0, y: 0, z: 0 }
     basic.showString("X")
     music.play(music.stringPlayable("A3 C4 E4 A4", 400), music.PlaybackMode.UntilDone)
 }
@@ -39,8 +37,8 @@ basic.forever(function () {
         drivingPackage.x = input.acceleration(Dimension.X)
         drivingPackage.y = input.acceleration(Dimension.Y)
         drivingPackage.z = input.acceleration(Dimension.Z)
-        drivingPackage.k = keySqequence[1]
-        radio.sendString(JSON.stringify(drivingPackage))
+        stringPackage = `${drivingPackage.x},${drivingPackage.y},${drivingPackage.z}`
+        radio.sendString(stringPackage)
     }
     basic.pause(200)
 })
