068945fbf96498cee41ae8881db8efc14d2ac5c9
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-05-19 20:27:24 +0200

    getting

--- main.ts
+++ main.ts
@@ -5,18 +5,21 @@ radio.setTransmitSerialNumber(true)
 control.deviceSerialNumber()
 
 type drivingSignal = {
+    k: number;
     x: number;
     y: number;
     z: number
 }
 
-let drivingPackage: drivingSignal = {x: 0, y: 0, z: 0 }
+let keySqequence: Array<number> = [4, 8, 6, 1, 5, 3, 4]
+
+let drivingPackage: drivingSignal = { k: keySqequence[0], x: 0, y: 0, z: 0 }
 let ready = false
 let serialNumber = control.deviceSerialNumber()
 
 
 function calibrate() {
-    drivingPackage = { x: 0, y: 0, z: 0 }
+    drivingPackage = { k: keySqequence[0], x: 0, y: 0, z: 0 }
     music.play(music.stringPlayable("A3 C4 E4 A4", 200), music.PlaybackMode.UntilDone)
     basic.showString("X")
 }
@@ -33,15 +36,14 @@ input.onButtonPressed(Button.B, function () {
 })
 
 
-basic.forever(function() {
-    if(ready) {
+basic.forever(function () {
+    if (ready) {
         basic.showString("R")
         drivingPackage.x = input.acceleration(Dimension.X)
         drivingPackage.y = input.acceleration(Dimension.Y)
         drivingPackage.z = input.acceleration(Dimension.Z)
-        drivingPackage = { x: 1, y: 1, z: 1 }
+        drivingPackage.k = keySqequence[1]
         radio.sendString(JSON.stringify(drivingPackage))
     }
     basic.pause(200)
 })
-
