2601e3bd2519dd875cf620c04be9176beec6ff68
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-06-16 09:47:10 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,41 +4,19 @@ radio.setFrequencyBand(39)
 radio.setTransmitSerialNumber(true)
 console.log(control.deviceSerialNumber())
 
-type drivingSignal = {
-    x: number;
-    y: number;
-    z: number
+let run: string = "run";
+let turn: string = "turn";
+
+function sendApply(state:string) {
+    radio.sendString(state)
+    basic.showString(state, 0)
 }
 
-let drivingPackage: drivingSignal = { x: 0, y: 0, z: 0 }
-let stringPackage: string
-let stopPackage: string
-let ready: boolean
-
-
 input.onButtonPressed(Button.A, function () {
-    ready = true
-    music.play(music.stringPlayable("C4 E4 G4 C5", 800), music.PlaybackMode.UntilDone)
+    sendApply(run)
 })
 
 input.onButtonPressed(Button.B, function () {
-    ready = false
-    basic.pause(10)
-    stopPackage = "stop"
-    radio.sendString(stopPackage)
-    basic.showString("X", 0)
-    music.play(music.stringPlayable("A3 C4 E4 A4", 800), music.PlaybackMode.UntilDone)
+    sendApply(turn)
     console.log(control.deviceSerialNumber())
-})
-
-basic.forever(function () {
-    if (ready) {
-        basic.showString("R", 0)
-        drivingPackage.x = -input.acceleration(Dimension.X)
-        drivingPackage.y = input.acceleration(Dimension.Y)
-        drivingPackage.z = input.acceleration(Dimension.Z)
-        stringPackage = `${drivingPackage.x},${drivingPackage.y},${drivingPackage.z}`
-        radio.sendString(stringPackage)
-    }
-    basic.pause(5)
 })
